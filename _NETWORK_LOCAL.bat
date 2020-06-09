fa88ae6b2147ae15b2a5a328848c7e0179c44463
KY-Jung <39116699+KY-Jung@users.noreply.github.com>
2020-06-09 15:47:20 +0900

    Add files via upload
    patch : local mode

---
+++ _NETWORK_LOCAL.bat
@@ -0,0 +1,10 @@
+rem regedit.exe /S REG_MODE_QR.reg
+
+reg add "HKCR\zADC" /v "SERVICE_NETWORK" /t REG_SZ /d "FALSE" /f
+
+taskkill /im ADClient.exe
+taskkill /F /IM ADClientService.exe
+
+timeout 6 > NUL
+
+start ADClient.exe
